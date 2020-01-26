fa26996c8f4a2da1a1ffc0a512bb558a72ece3ce
leonixyz <leonixyz@gmail.com>
2020-01-26 19:46:48 +0100

    replace responsive images after a timout due to weird mobile browser behaviour

--- src/responsive-images/index.js
+++ src/responsive-images/index.js
@@ -5,14 +5,17 @@
     const target = breakpoints.find(b => b > screenWidth);
     const heroes = document.getElementsByClassName('hero');
     for(let hero of heroes) {
-      let bgImageCss = window.getComputedStyle(hero).backgroundImage;
-      bgImageCss = bgImageCss.replace('placeholders', target);
-      bgImage = bgImageCss.replace('url("', '').replace('")', '');
-      const img = new Image();
-      img.src = bgImage;
-      img.addEventListener('load', event => {
-        hero.style.backgroundImage = bgImageCss;
-      });
+      setTimeout(()=> {
+        const style = window.getComputedStyle(hero);
+        let bgImageCss = style.backgroundImage;
+        bgImageCss = bgImageCss.replace('placeholders', target);
+        const bgImage = bgImageCss.replace('url("', '').replace('")', '');
+        const img = new Image();
+        img.src = bgImage;
+        img.addEventListener('load', event => {
+          hero.style.backgroundImage = bgImageCss;
+        });
+      }, 200);
     }
   });
 })();
