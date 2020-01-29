5b223bc54438e090e022a5b669aa9c8eef0f03d9
leonixyz <leonixyz@gmail.com>
2020-01-29 15:10:05 +0100

    show IE users custom messages to update their browser

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-require('./my-contact-form/index.js');
-require('./my-image-gallery/index.js');
 require('./responsive-images/index.js');
+
+if(/MSIE \d|Trident.*rv:/.test(navigator.userAgent)) {
+  // Internet Explorer and old MS Edge
+  const ieOnly = document.querySelectorAll('.IE-only');
+  const nonIeOnly = document.querySelectorAll('.non-IE-only');
+  ieOnly.forEach(e => {
+    e.classList.toggle('IE-only')
+  });
+  nonIeOnly.forEach(e => {
+    e.hidden = true;
+  });
+} else {
+  // Normal browsers
+  require('./my-contact-form/index.js');
+  require('./my-image-gallery/index.js');
+}
