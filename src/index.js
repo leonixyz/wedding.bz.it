deef6c9b39ac8e79faf786cbd44b578c0073b3a2
leonixyz <leonixyz@gmail.com>
2020-02-05 15:45:34 +0100

    load polyfill for IntersectionObserver on IE and old Edge

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-require('./responsive-images/index.js');
-
+// Load polyfill for IntersectionObserver on Internet Explorer and old MS Edge
 if(/MSIE \d|Trident.*rv:/.test(navigator.userAgent)) {
-  // Internet Explorer and old MS Edge
-  const ieOnly = document.querySelectorAll('.IE-only');
-  const nonIeOnly = document.querySelectorAll('.non-IE-only');
-  ieOnly.forEach(e => {
-    e.classList.toggle('IE-only')
-  });
-  nonIeOnly.forEach(e => {
-    e.hidden = true;
-  });
-} else {
-  // Normal browsers
-  require('./my-contact-form/index.js');
-  require('./my-image-gallery/index.js');
+  var request = new XMLHttpRequest();
+  request.open('GET', 'https://polyfill.io/v3/polyfill.min.js?features=IntersectionObserver', false);
+  request.send(null);
+
+  if (request.status === 200) {
+    eval(request.responseText);
+  }
 }
+
+require('./responsive-images/index.js');
+require('./my-contact-form/index.js');
+require('./my-image-gallery/index.js');
