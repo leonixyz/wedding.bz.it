c8dd9618b1db1228f190ffff1ce872a7f2b8c96d
leonixyz <leonixyz@gmail.com>
2020-02-05 15:13:16 +0100

    replace my-image-gallery custom element with old-fashioned code to allow the website to work on older browsers

--- src/my-image-gallery/index.js
+++ src/my-image-gallery/index.js
@@ -1,94 +1,60 @@
 (() => {
-  if (!customElements) return;
-  customElements.define('my-image-gallery',
-    /**
-     * An image gallery
-     */
-    class MyImageGallery extends HTMLElement {
+  document.addEventListener('DOMContentLoaded', (event) => {
+    // get the main element
+    const ui = document.getElementById('my-image-gallery');
 
-      /**
-       * Observed attributes
-       */
-      static get observedAttributes() {
-        return [
-          'images',
-        ];
-      }
+    // Get image list as an attribute
+    const images = JSON.parse(ui.getAttribute('data-images')) || [];
 
+    // Create an overlay for showing the images bigger
+    const overlay = ui.querySelector('#overlay');
+    overlay.addEventListener('click', event => {
+      overlay.classList.toggle('hidden');
+    });
+    const overlayContent = ui.querySelector('#overlay-content');
 
-      /**
-       * Setup custom element
-       */
-      constructor () {
-        // Always call super() first in constructor.
-        super();
+    // Render images
+    const masonry = document.createElement('div');
+    masonry.classList.add('masonry');
 
-        // Create the template and attach it as a shadow DOM - will be available as this.shadowRoot
-        const css = require('./style.css').default;
-        const html = require('./template.html').default;
-        const template = document.createElement('template');
-        template.innerHTML = `<style>${css}</style>${html}`;
-        this.attachShadow({ mode: 'open' })
-          .appendChild(template.content.cloneNode(true));
-      }
+    images.forEach(img => {
+      const i = document.createElement('img');
+      i._src = img.preview;
+      i.alt = img.alt;
 
+      // add onclick callback on each image to trigger the overlay
+      i.addEventListener('click', event => {
+        const largeImage = document.createElement('img');
+        largeImage.src = img.url;
+        largeImage.alt = img.alt;
 
-      /**
-       * Render element and setup overlay
-       */
-      connectedCallback () {
-        // Get image list as an attribute
-        this.images = JSON.parse(this.getAttribute('images')) || [];
+        overlayContent.innerHTML = '';
+        overlayContent.appendChild(largeImage);
 
-        // Create an overlay for showing the images bigger
-        const overlay = this.shadowRoot.querySelector('#overlay');
-        overlay.addEventListener('click', event => {
-          overlay.classList.toggle('hidden');
-        });
-        const overlayContent = this.shadowRoot.querySelector('#overlay-content');
+        overlay.classList.toggle('hidden');
+      });
 
-        // Render images
-        const masonry = document.createElement('div');
-        masonry.classList.add('masonry');
-
-        this.images.forEach(img => {
-          const i = document.createElement('img');
-          i._src = img.preview;
-          i.alt = img.alt;
-
-          // add onclick callback on each image to trigger the overlay
-          i.addEventListener('click', event => {
-            const largeImage = document.createElement('img');
-            largeImage.src = img.url;
-            largeImage.alt = img.alt;
-
-            overlayContent.innerHTML = '';
-            overlayContent.appendChild(largeImage);
-
-            overlay.classList.toggle('hidden');
-          });
-
-          // load image previews lazily
-          new IntersectionObserver(
-            entries => {
-              entries.forEach(entry => {
-                if (entry.isIntersecting) {
-                  entry.target.src = entry.target._src;
-                }
-              });
-            },
-            {
-              root: null,
-              rootMargin: '500px',
-              threshold: 0
+      // load image previews lazily
+      new IntersectionObserver(
+        entries => {
+          entries.forEach(entry => {
+            if (entry.isIntersecting) {
+              entry.target.src = entry.target._src;
             }
-          ).observe(i);
+          });
+        },
+        {
+          root: null,
+          rootMargin: '1000px',
+          threshold: 0
+        }
+      ).observe(i);
 
-          masonry.appendChild(i);
-        });
+      masonry.appendChild(i);
+    });
 
-        this.shadowRoot.appendChild(masonry);
-      }
-    }
-  );
+    ui.appendChild(masonry);
+
+
+  });
 })();
